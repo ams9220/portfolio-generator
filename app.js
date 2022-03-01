8a86e0fbe334b45d0e7bf5ad3a23f4fc14a96650
Ana <amsanc001@gmail.com>
2022-03-01 12:53:02 -0500

    generate HTML file

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//    const printProfileData = profileDataArr => {
+//        // This...
+//        for (let i = 0; i < profileDataArr.length; i += 1) {
+//        console.log(profileDataArr[i]);
+//        }
+//    
+//        console.log('================');
+//    
+//        // Is the same as this...
+//        profileDataArr.forEach(profileItem => console.log(profileItem));
+//    };
+//    printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+
+//    const generatePage = () => 'Name: Jane, Github: janehub';
+//    console.log(generatePage());
