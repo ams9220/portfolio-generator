cb079038c725c33b973862555ac62fce35deb25c
Ana <amsanc001@gmail.com>
2022-03-02 17:21:56 -0500

    html output'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -145,9 +146,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
+     const pageHTML = generatePage(portfolioData);
 
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  });
 
 
 
