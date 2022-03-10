e0c937aa442b6e71e8ab3a2ba2b35eec4efda01a
Ana <amsanc001@gmail.com>
2022-03-10 13:40:52 -0500

    copyFile updated

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -19,8 +19,8 @@ const writeFile = fileContent => {
     });
 };
 
-const copyFile = fileContent => {
-    return new Promise(() => {
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
         fs.copyFile('./src/style.css', './dist/style.css', err => {
             // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
             if (err) {
@@ -32,13 +32,10 @@ const copyFile = fileContent => {
             // if everything went well, resolve the Promise and send the successful data to the `.then()` method
             resolve({
             ok: true,
-            message: 'Style sheet copied successfully!'
+            message: 'Stylesheet created!'
             });
         });
     });
 };
 
 module.exports = { writeFile, copyFile };
-
-
-
